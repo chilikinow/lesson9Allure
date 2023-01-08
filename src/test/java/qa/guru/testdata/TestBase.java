78db049fabdc46c3425ef7903818e73b0ea388c3
Чиликин Олег Владимирович <ChilikinOV@gnivc.ru>
2023-01-08 17:51:42 +0300

    add chromedriver to resources

--- src/test/java/qa/guru/testdata/TestBase.java
+++ src/test/java/qa/guru/testdata/TestBase.java
@@ -12,10 +12,12 @@ public class TestBase {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
-        capabilities.setCapability(CapabilityType.BROWSER_VERSION, "102.0");
+        capabilities.setCapability(CapabilityType.BROWSER_VERSION, "102.0"); //Version=102.0.5005.61
 
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browserSize = "1920x1080";
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver"); //Version=102.0.5005.61
     }
 }
