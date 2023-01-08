0ef6ef6db1a84ef95f8129c83dfc40c96c8eda4f
Чиликин Олег Владимирович <ChilikinOV@gnivc.ru>
2023-01-08 17:18:35 +0300

    add DesiredCapabilities to TestBase

--- src/test/java/qa/guru/testdata/TestBase.java
+++ src/test/java/qa/guru/testdata/TestBase.java
@@ -2,11 +2,19 @@ package qa.guru.testdata;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        capabilities.setCapability(CapabilityType.BROWSER_VERSION, "102.0");
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browserSize = "1920x1080";
     }
