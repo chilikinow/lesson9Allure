e8cebc49e9a09b4c74f90f7660040b6ae40fc9e3
Чиликин Олег Владимирович <ChilikinOV@gnivc.ru>
2023-01-09 20:37:43 +0300

    changes

--- src/test/java/qa/guru/testdata/TestBase.java
+++ src/test/java/qa/guru/testdata/TestBase.java
@@ -9,13 +9,13 @@ public class TestBase {
     static void beforeAll() {
 
 //        Configuration.remote = "http://selenoid:4444/wd/hub";
-        Configuration.remote = "http://localhost:4444/wd/hub/";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browserVersion = "104.0";
+        Configuration.remote = "http://localhost:4444/wd/hub";
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.browserVersion = "104.0";
 
 //        Configuration.browser = "chrome";
-        System.setProperty("selenide.browser", "Chrome");
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe"); //104.0.5112.79//Version=102.0.5005.61 for linux
+//        System.setProperty("selenide.browser", "Chrome");
+//        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe"); //104.0.5112.79//Version=102.0.5005.61 for linux
 //        System.setProperty("webdriver.chrome.driver", "C:\workspace\lesson9Allure\driver\chromedriver.exe");
 
     }
